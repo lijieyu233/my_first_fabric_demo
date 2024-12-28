4dadf389b6bc33768276cebf39850ab1a64b1112
lijieyu <2418141009@qq.com>
2024-12-28 19:37:59 +0800

    第一次提交

---
+++ src/main/java/name/demo_mode/DemoModeDataGenerator.java
@@ -0,0 +1,11 @@
+package name.demo_mode;
+
+import net.fabricmc.fabric.api.datagen.v1.DataGeneratorEntrypoint;
+import net.fabricmc.fabric.api.datagen.v1.FabricDataGenerator;
+
+public class DemoModeDataGenerator implements DataGeneratorEntrypoint {
+	@Override
+	public void onInitializeDataGenerator(FabricDataGenerator fabricDataGenerator) {
+
+	}
+}
---
+++ src/main/java/name/demo_mode/DemoModelClient.java
@@ -0,0 +1,10 @@
+package name.demo_mode;
+
+import net.fabricmc.api.ClientModInitializer;
+
+public class DemoModelClient implements ClientModInitializer {
+    @Override
+    public void onInitializeClient() {
+
+    }
+}
